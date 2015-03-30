91e53af47fde3d5a14ee930ba89276e8081577b3
daviess <sergio.davies@gmail.com>
2015-03-30 11:06:19 +0100

    buffer input C code currently being written, python code being adapted to handle output data buffering - merge needed with master

--- spynnaker/pyNN/buffer_management/buffer_receiving_to_host_manager.py
+++ spynnaker/pyNN/buffer_management/buffer_receiving_to_host_manager.py
@@ -1,3 +1,5 @@
+from spinnman.messages.eieio.command_objects.spinnaker_request_read_data import \
+    SpinnakerRequestReadData
 from spynnaker.pyNN.buffer_management.abstract_buffer_manager import \
     AbstractBufferManager
 
@@ -5,13 +7,16 @@ from spynnaker.pyNN.buffer_management.abstract_buffer_manager import \
 class BufferReceivingToHostManager(AbstractBufferManager):
 
     def __init__(self, placements, routing_key_infos, graph_mapper,
-                 port, local_host, transceiver):
+                 port, local_host, transceiver, buffer_manager):
         self._receive_vertices = dict()
+        self._buffer_manager = buffer_manager
+        self._buffer_manager.register_listener(
+            SpinnakerRequestReadData, self.packet_listener)
         AbstractBufferManager.__init__(
             self, placements, routing_key_infos, graph_mapper, port,
             local_host, transceiver)
 
-    def add_received_vertex(self, manageable_vertex):
+    def add_receiver_vertex(self, manageable_vertex):
         """ adds a partitioned vertex into the managed list for vertices
         which require buffers to be extracted from them during runtime
 
@@ -25,3 +30,6 @@ class BufferReceivingToHostManager(AbstractBufferManager):
                 self._placements.get_placement_of_subvertex(vertex)
             self._receive_vertices[(placement.x, placement.y, placement.p)] = \
                 vertex
+
+    def packet_listener(self, packet):
+        pass
